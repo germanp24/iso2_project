9a4cfe01b6c4a59d0adb1ee3df0769794701e607
German Pajarero <germanpajareropino@gmail.com>
2024-10-11 19:28:56 +0200

    Added login page (controller, entity and HTML) (Not working, only visual)

---
+++ delivery/src/main/java/es/uclm/delivery/business/entity/Login.java
@@ -0,0 +1,23 @@
+package es.uclm.delivery.business.entity;
+
+public class Login {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
