133757fb9525f4eb5e9ec6d788894dfd90c840c4
“German <germanpajareropino@gmail.com>
2024-11-05 11:14:07 +0100

    Added login (alpha version)

---
+++ delivery/src/main/java/es/uclm/delivery/business/controller/LoginController.java
@@ -0,0 +1,23 @@
+package es.uclm.delivery.business.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import es.uclm.delivery.business.entity.Login;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String loginForm(Model model) {
+        model.addAttribute("login", new Login());
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String greetingSubmit(@ModelAttribute Login login, Model model) {
+        model.addAttribute("login", login);
+        return "result";
+    }
+}
